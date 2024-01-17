18081a103086731edf3eb87a368c1cbb3f2afec1
Anse98 <lorenzo.anselmi98.com>
2024-01-17 17:21:30 +0100

    ADD- funzione dentro load per permettere la corretta visualizzazione dei post correllati nella show

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -22,7 +22,11 @@ class ProjectController extends Controller
     public function show(Project $project)
     {
         // Aggiungere qua type.projects, serve per recuperare anche tutti i progetti che hanno quel type
-        $project->load('type', 'technologies', 'type.projects', 'type.projects.technologies');
+
+        // La function in type.projects serve in questo caso per escludere l'id del progetto in types con lo stesso id del progetto, in poche parole 'prendimi tutti i dati tranne quelli dove id(di type.projects) è diverso dall'id del progetto'. 
+        $project->load(['type', 'technologies', 'type.projects' => function ($query) use ($project) {
+            $query->where('id', '<>', $project->id);
+        }, 'type.projects.technologies']);
 
         return response()->json([
             'project' => $project,
